0ff10012cb5c997f916d5832c8615bed2fbcfa73
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-12-20 21:43:12 -0800

    exit

--- shell.h
+++ shell.h
@@ -169,4 +169,16 @@ char *integer_converter2(unsigned int count);
 unsigned int sig_flag;
 
 void print_message1(char *str);
+/**
+ * struct list_path - singly linked list
+ * @dir: string - (malloc'ed string)
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ * for directories of PATH
+ */
+typedef struct list_path
+{
+char *dir;
+struct list_path *next;
+} list_p;
 #endif /* _SHELL_H_ */
