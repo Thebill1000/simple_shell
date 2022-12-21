cc619eb26b76401f255ba3e02c630f8d06a3f103
Aseel3640 <aseelsaifaldin@yahoo.com>
2022-12-20 21:50:59 -0800

    exit

--- shell.h
+++ shell.h
@@ -184,4 +184,10 @@ struct list_path *next;
 char *_which(char **commands, char **env);
 list_p *list_path(char **env);
 void free_list(list_p *head);
+
+int is_chain(info_t *info, char *buf, size_t *p);
+void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len);
+int replace_alias(info_t *info);
+int replace_vars(info_t *info);
+int replace_string(char **old, char *new);
 #endif /* _SHELL_H_ */
